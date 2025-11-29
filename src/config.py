23a9a5dae079074a5348707325ec91a3b10105cb
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2025-11-29 23:43:37 +0300

    config done

---
+++ src/config.py
@@ -0,0 +1,7 @@
+BANK_APPS = {
+    "Commercial Bank of Ethiopia": "com.combanketh.mobilebanking",
+    "Bank of Abyssinia": "com.boa.mobilebanking",
+    "Dashen Bank": "com.dashen.mobile"
+}
+
+SOURCE = "Google Play Store"
